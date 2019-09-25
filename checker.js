ec662e8402ad742fff0ed00f25179b01a188260e
Antonette <amgichohu@gmail.com>
2019-09-25 09:45:37 -0500

    exercise 1

--- checker.js
+++ checker.js
@@ -1,4 +1,24 @@
-function validateInput(password, verifier) 
+validateInput =function(password, verifier) 
 {
+    var password = document.getElementById('password').value
+    var verifier = document.getElementById('verifier').value
+
+    console.log(password);
+    console.log(verifier);
     
+    password=password.toString()
+    verifier=verifier.toString()
+
+    
+
+
+    if(password!=verifier)
+    {
+        alert("passwords must match!")
+    }
+    else if(password.toString().length <8)
+    {
+        alert("Password must be at least 8 characters long!")
+    }
+
 }
