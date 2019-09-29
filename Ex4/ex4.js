a67bb98ad6f78cf9154a99743a093147b6231a09
Antonette <amgichohu@gmail.com>
2019-09-29 16:12:52 -0500

    ex 4 upload

--- Ex4/ex4.js
+++ Ex4/ex4.js
@@ -1,5 +1,29 @@
 changer = function()
 {
-    color = document.getElementById("border").value
-    
+    console.log("it runs")
+    br = document.getElementById("borderRed").value
+    bg = document.getElementById("borderGreen").value
+    bb = document.getElementById("borderBlue").value
+
+    bw = document.getElementById("borderWidth").value
+   
+
+    bgr = document.getElementById("backgroundRed").value
+    console.log("background red val", bgr)
+    bgg = document.getElementById("backgroundGreen").value
+    console.log("background green", bgg)
+    bgb = document.getElementById("backGroundBlue").value
+    console.log("background blue", bgb);
+   
+
+    // backgroundDiv = document.getElementById("content")
+    // console.log("background div", backgroundDiv)
+
+    document.getElementById("content").style.border.width = bw + "dotted" + 'rgb(' + [br,bg,bb].join(',') + ')';
+
+    document.getElementById("content").style.border.color = 'rgb(' + [br,bg,bb].join(',') + ')'
+
+
+    document.getElementById("content").style.backgroundColor = 'rgb(' + [bgr,bgg,bgb].join(',') + ')';
+    // (bgr, bgg, bgb);
 }
